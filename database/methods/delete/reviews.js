130fa3afd1d0c85e58e8824bbc1076fbccd57b95
nathanq95 <nathanquiocson@gmail.com>
2020-11-18 00:00:40 -0800

    added functions to update review summaries

--- database/methods/delete/reviews.js
+++ database/methods/delete/reviews.js
@@ -1,12 +1,15 @@
 const ReviewModel = require('../../models/reviews.js');
+const reviewSummaryMethods = require('../update/reviewsummary.js');
 const counterMethods = require('../counter.js');
 
 const deleteReview = (reviewId) => new Promise((resolve, reject) => {
+    reviewSummaryMethods.decrementRating(reviewId);
     counterMethods.decrementReviewSeq()
     .then((counter) => {
         resolve(ReviewModel.findOneAndDelete({review_id: reviewId}));
     })
     .catch((err) => reject(err));
+
 });
 
 module.exports = {
