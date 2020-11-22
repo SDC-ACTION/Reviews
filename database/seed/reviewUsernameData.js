356b43863c0f2501454ea1b9b5b7e9c27a6ea905
Nathan <nathanquiocson@Nathans-MacBook-Pro.local>
2020-11-22 01:25:40 -0800

    refactored sample data

--- database/seed/reviewUsernameData.js
+++ database/seed/reviewUsernameData.js
@@ -2,8 +2,8 @@ module.exports = [
     'FasterThanFast1', 'BillyBob4', 'NoProblem321', 'Ace', 'WhatThe5',
     'GodlyOfGods', 'abc123', 'Shortcake7', 'IHateEverything', '45Minutes',
     '52Cards', 'HelloKitty5', 'Driller40', 'Muppets', 'Slinky', 
-    'EliteDarkLord_', 'DragonSlayer_3.14159', 'brockObama', 'KanyeEast', 'past', 
-    'bigJon', 'theMnth', 'daGrownUp', 'A$AP Bullwinkle', 'Tyler_the_destroyer',
+    'EliteDarkLord_', 'DragonSlayer_3', 'brockObama', 'KanyeEast', 'past', 
+    'bigJon', 'theMnth', 'daGrownUp', 'A$AP_Bullwinkle', 'Tyler_the_destroyer',
     'aubrey', 'FKABranches', '3pac', 'rexRiversideCounty', '22savage',
     'kingjames', 'bronny', 'mkbsd', 'bill', 'dave3d',
     'zo', 'gelo', 'melo', 'lavar', 'tina'
