dc0bf0cce1f0092f30a6f3bc67713aa77dc4b4e2
nathanq95 <nathanquiocson@gmail.com>
2020-11-24 00:27:42 -0800

    updated seeder to populate reviews & review_summaries tables

--- database/seed/postgres/seedRandomData.js
+++ database/seed/postgres/seedRandomData.js
@@ -2,6 +2,7 @@ const { Client } = require('pg');
 const usersData = require('../reviewUsernameData.js');
 const reviewHeadingData = require('../reviewHeadingData.js');
 const reviewTextData = require('../reviewTextData.js');
+const reviews = require('../../models/reviews.js');
 
 const client = new Client({
     user: 'admin',
@@ -18,43 +19,76 @@ client.connect()
 });
 
 const seed = async () => {
-    let max = 15000000;
-    let min = 10000000;
-    let products = Math.floor(Math.random() * min);
-    let reviews = Math.floor(Math.random() * (max-products) + products);
-
-    while (products + reviews < min) {
-         products = Math.floor(Math.random() * min);
-         reviews = Math.floor(Math.random() * (max-products) + products);
-    }
+    let products = 170000;
   
     await client.query('DELETE FROM review_summaries WHERE summary_id >= 0');
     await client.query('DELETE FROM reviews WHERE review_id >= 0');
     await client.query('BEGIN');
-    await client.query('PREPARE initialize_summaries (int, int) AS INSERT INTO review_summaries VALUES ($1, $2)')
-
+    await client.query('PREPARE populate_summaries (int, int, int, int, int, int, int, int) AS INSERT INTO review_summaries VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING product_id, rating_1, rating_2, rating_3, rating_4, rating_5');
+    await client.query('PREPARE insert_review (int, int, char(30), char(30), char(30), int) AS INSERT INTO reviews VALUES($1, $2, $3, $4, $5, $6)');
+    
+    let reviewId = 0;
     for (let i = 0; i < products; i++) {
-        await client.query(`EXECUTE initialize_summaries(${i}, ${i})`);
-        process.stdout.write(`loading: ${i} / ${products + reviews}\r`);
-    }
-    
-    process.stdout.clearLine();
-    console.log('Summaries initialized!');
-    await(client.query('PREPARE insert_review (int, int, char(30), char(30), char(30), int) AS INSERT INTO reviews VALUES($1, $2, $3, $4, $5, $6)'));
-    
-    for (let i = 0; i < reviews; i++) {
-        let productId = Math.floor(Math.random() * products);
-        let userId = Math.floor(Math.random() * usersData.length);
-        let reviewRating = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
-        
-        await client.query(`EXECUTE insert_review(${i}, ${productId}, '${usersData[userId]}', '${reviewHeadingData[Math.floor(Math.random() * reviewHeadingData.length)]}', '${reviewTextData[Math.floor(Math.random() * reviewTextData.length)]}', ${reviewRating})`);
-        process.stdout.write(`loading: ${products + i} / ${products + reviews}\r`);
+        let maxRatingQuantity = Math.floor(Math.random() * (6-1) + 1);
+        let rating1 = 0, rating2 = 0, rating3 = 0, rating4 = 0, rating5 = 0;
+        let ratingQuantity;
+        while (rating1 + rating2 + rating3 + rating4 + rating5 < maxRatingQuantity) {
+            let rating = Math.floor(Math.random() * 5);
+            if(rating == 1) {
+                rating1 += 1;
+            } else if(rating == 2) {
+                rating2 += 1;
+            } else if(rating == 3) {
+                rating3 += 1;
+            } else if(rating == 4) {
+                rating4 += 1;
+            } else {
+                rating5 += 1;
+            }
+
+        }
+
+        ratingQuantity = rating1+rating2+rating3+rating4+rating5;
+        await client.query(`EXECUTE populate_summaries(${i}, ${i}, ${rating1}, ${rating2}, ${rating3}, ${rating4}, ${rating5}, ${ratingQuantity})`)
+        .then( async (data) => {
+            process.stdout.write(`loading: ${i+reviewId}\r`);
+            for (let j = 0; j < ratingQuantity; j++) {
+                let userId = Math.floor(Math.random() * usersData.length);
+                let headingText = Math.floor(Math.random() * reviewTextData.length);
+                        if(rating1 > 0) {
+                            await client.query(`EXECUTE insert_review(${reviewId}, ${data.rows[0].product_id}, '${usersData[userId]}', '${reviewHeadingData[headingText]}', 'text', ${1})`)
+                            reviewId++;
+                            rating1--;
+                        } else if(rating2 > 0) {
+                            await client.query(`EXECUTE insert_review(${reviewId}, ${data.rows[0].product_id}, '${usersData[userId]}', '${reviewHeadingData[headingText]}', 'text', ${2})`)
+                            reviewId++;
+                            rating2--;
+                        } else if(rating3 > 0) {
+                            await client.query(`EXECUTE insert_review(${reviewId}, ${data.rows[0].product_id}, '${usersData[userId]}', '${reviewHeadingData[headingText]}', 'text', ${3})`)
+                            reviewId++;
+                            rating3--;
+                        } else if(rating4 > 0) {
+                            await client.query(`EXECUTE insert_review(${reviewId}, ${data.rows[0].product_id}, '${usersData[userId]}', '${reviewHeadingData[headingText]}', 'text', ${4})`)
+                            reviewId++;
+                            rating4--;
+                        } else {
+                            await client.query(`EXECUTE insert_review(${reviewId}, ${data.rows[0].product_id}, '${usersData[userId]}', '${reviewHeadingData[headingText]}', 'text', ${5})`)
+                            reviewId++;
+                            rating5--;
+                        }
+                        process.stdout.write(`loading: ${i + reviewId}\r`);
+                    }
+                })
+                .catch((err) => {
+                    console.log('ERROR POPULATING SUMMARIES: ', err);
+                });
     }
 
     process.stdout.clearLine();
     await client.query('END');
-    console.log('reviews table seeded!');
-    console.log(`${products + reviews} records loaded to the database!`);
+    console.log(`${reviewId} reviews added!`);
+    console.log(`${products} summaries added!`);
+    console.log(`${products+reviewId} total records added!`);
     process.exit(0);
 }
 
