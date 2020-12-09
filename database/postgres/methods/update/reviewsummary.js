b03b178b65247dbea8f1ce45c9bd22abd8b40cfe
Nathan <nathanquiocson@Nathans-MacBook-Pro.local>
2020-12-09 15:55:16 -0800

    cleaned up syntax

--- database/postgres/methods/update/reviewsummary.js
+++ database/postgres/methods/update/reviewsummary.js
@@ -1,23 +1,10 @@
 const client = require('../../index.js');
 
-const updateRating = async (ratingData, newRating) => {
-    let newData = `rating_${newRating} = rating_${newRating} + 1, rating_${ratingData.review_rating} = rating_${ratingData.review_rating} - 1`;
-    let updatedRating = await client.query(`UPDATE review_summaries SET ${newData} WHERE product_id = ${ratingData.product_id}`);
+const updateRating = async (currentData, newRating) => await client.query(`UPDATE review_summaries SET rating_${newRating} = rating_${newRating} + 1, rating_${currentData.review_rating} = rating_${currentData.review_rating} - 1 WHERE product_id = ${currentData.product_id}`);
 
-    return updatedRating;
-};
+const incrementRating = async (productId, reviewRating) => await client.query(`UPDATE review_summaries SET rating_${reviewRating} = rating_${reviewRating} + 1, total_reviews = total_reviews + 1 WHERE product_id = ${productId}`);
 
-const incrementRating = async (productId, reviewRating) => {
-    let newRating = await client.query(`UPDATE review_summaries SET rating_${reviewRating} = rating_${reviewRating} + 1, total_reviews = total_reviews + 1 WHERE product_id = ${productId}`);
-
-    return newRating
-};
-
-const decrementRating = async (productId, reviewRating) => {
-    let decrementedRating = await client.query(`UPDATE review_summaries SET rating_${reviewRating} = rating_${reviewRating} - 1, total_reviews = total_reviews - 1 WHERE product_id = ${productId}`);
-
-    return decrementedRating;
-};
+const decrementRating = async (productId, reviewRating) => await client.query(`UPDATE review_summaries SET rating_${reviewRating} = rating_${reviewRating} - 1, total_reviews = total_reviews - 1 WHERE product_id = ${productId}`);
 
 module.exports = {
     updateRating,
