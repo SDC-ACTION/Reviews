c76755be358a5622119f6a81fbe39711eef771a2
Nathan <nathanquiocson@Nathans-MacBook-Pro.local>
2020-12-08 21:12:28 -0800

    added function to update review_summaries table

--- database/postgres/methods/reviews.js
+++ database/postgres/methods/reviews.js
@@ -1,4 +1,13 @@
 const client = require('../index.js');
+const { incrementRating } = require('../methods/update/reviewsummary.js');
+
+const addReview = async (reviewData) => {
+    let newReview = await client.query(`INSERT INTO reviews (product_id, username, review_heading, review_text, review_rating) VALUES (${reviewData.product_id}, '${reviewData.username}', '${reviewData.review_heading}', '${reviewData.review_text}', ${reviewData.review_rating})`);
+
+    await incrementRating(reviewData.product_id, reviewData.review_rating);
+
+    return newReview
+};
 
 const getReviews = async (productId) => {
     let reviews = await client.query(`SELECT * FROM reviews WHERE product_id = ${productId}`);
@@ -7,5 +16,6 @@ const getReviews = async (productId) => {
 };
 
 module.exports = {
+    addReview,
     getReviews
 }
