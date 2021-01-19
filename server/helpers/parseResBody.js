898603eb6602e4a277c2efef72a5ae5789e4ff6b
Nathan <nathanquiocson@Nathans-MacBook-Pro.local>
2021-01-19 14:19:23 -0800

    updated checkCache

--- server/helpers/parseResBody.js
+++ server/helpers/parseResBody.js
@@ -17,15 +17,12 @@ const parseResBody = (req, res, next) => {
       if (chunk){
           chunks.push(chunk);
       }
-      if (typeof chunks[0] !== 'string' && chunks[0].toString('utf8') !== 'OK' && res.statusCode !== 404 && res.statusCode !== 500) {
-        resBody = JSON.parse(Buffer.concat(chunks));
-        if (resBody.reviews) {
-          saveToCache(req.params.product_id, resBody);
-        }
-
-        if (resBody.rows) {
-          deleteFromCache(resBody.rows[0].product_id, res);
-        }
+      resBody = JSON.parse(Buffer.concat(chunks));
+      if (resBody.reviews) {
+        saveToCache(req.params.product_id, resBody);
+      }
+      if (resBody.rows) {
+        deleteFromCache(resBody.rows[0].product_id, res);
       }
       oldEnd.apply(res, arguments);
   };
--- server/models/checkCache.js
+++ server/models/checkCache.js
@@ -1,16 +1,26 @@
 const memcached = require('./index.js');
-const { saveToCache } = require('./saveToCache.js');
 const { parseResBody } = require('../helpers/parseResBody.js');
+let cacheCount = 0;
+let newCount = 0;
+
 const checkCache = (req, res, next) => {
     memcached.get(req.params.product_id, (err, data) => {
         if (err){
             console.log('Error getting cached data: ', err);
         }
         else if (data){
+            cacheCount++;
+            process.stdout.write(`req handled by cache: ${cacheCount}`);
+            process.stdout.write(` req handled by service: ${newCount}\r`);
             res.json(data);
         }
         else {
-            parseResBody(req, res, next);
+            newCount++;
+            process.stdout.write(`req handled by cache: ${cacheCount}`);
+            process.stdout.write(` req handled by service: ${newCount}\r`);
+            if (res.statusCode !== 404 && res.statusCode !== 500){
+                parseResBody(req, res, next);
+            }
         }
     });
 };
